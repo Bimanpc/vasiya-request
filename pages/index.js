e7619a91dccba447137f47d70acd51007cea4f64
Βασίλης Ψαρομάτης PC TECH GR EU Vasiliy PS <77292453+Bimanpc@users.noreply.github.com>
2023-07-06 19:29:29 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Toolbox</title>
+        <title>Next.js Toolbox 2023 vasiliys 2023</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
